4bf1329d74ca876da2a780dcadf727e86c68cc93
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-12-18 22:33:00 +0100

    Created EntryPage Component

---
+++ src/components/EntryPage.jsx
@@ -0,0 +1,29 @@
+import { useState, Fragment } from "react";
+
+function EntryPage() {
+  return (
+    <div className="entry-container">
+      {/* Header section */}
+      <header className="header">wwww.resumifyapp.com</header>
+
+      {/* Main content section */}
+      <main className="entry-main">
+        <h1 className="heading-text">Welcome to Resumify</h1>
+        <h2 className="heading-subtext">
+          Build beautiful resumes easily with our app and land your dream job
+        </h2>
+        <button type="button" className="start-btn">
+          Get Started
+        </button>
+      </main>
+
+      {/* Footer section */}
+      <footer className="footer">
+        <a href="http://">@stefanbrkic1</a>
+        <div className="github-logo"></div>
+      </footer>
+    </div>
+  );
+}
+
+export default EntryPage;
